630a041b4486abcf4355d2012c2aef3f3c872e5d
libyano93ly <younus93ly@gmail.com>
2022-08-01 11:54:13 +0200

    Initialize Nested Object

--- Nested-Types/Program.cs
+++ Nested-Types/Program.cs
@@ -7,24 +7,35 @@ namespace Nested_Types
     {
         static void Main(string[] args)
         {
-         
-
+            Employee e1 = new Employee();
+            Console.WriteLine(e1.Employeeinsurance.CompanyName);
+          
             Console.ReadKey();
         }
     }
-
-    class Person
+    class Employee
     {
-        private int x;
-        public int y;
-    }
+        public int Id { get; set; }
+        public string Name { get; set; }
 
-    class A
-    {
-        class B
+        public Insurance Employeeinsurance { get; set; }
+
+        public Employee() => Employeeinsurance = new Insurance { PloicyId = -1, CompanyName = "N/A" };
+
+
+        public class Insurance
         {
-
+            public int PloicyId { get; set; }
+            public string CompanyName { get; set; }
         }
     }
-  
+
+    class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+
 }
